66b06c2a78ec2be559b345024ee7ca639b465c7f
babusanka <saintsbabu@gmail.com>
2020-07-29 00:04:03 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express')
+var app = express()
+var serv = require('http').Server(app)
+
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/client/index.html')
+})
+
+app.use('client', express.static(__dirname + '/client'))
+
+serv.listen(2000)
+console.log("Server Starded. (finally)")
+
+var SOCKET_LIST = {}
+
+var io = require('socket.io')(serv,{})
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random()
+    socket.x = 0
+    socket.y = 0
+    socket.color = getRandomColor()
+    SOCKET_LIST[socket.id] = socket
+
+
+
+})
+
+
+setInterval(function(){
+    var pack = []
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i]
+        socket.x++
+        socket.y++
+        pack.push({
+            x:socket.x,
+            y:socket.y,
+            color: socket.color
+        }) 
+            
+
+    }
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i]
+        socket.emit('newPositions',pack)
+    
+    }    
+
+
+
+
+
+
+
+
+        
+    
+}, 1000/25)
+
+let colors = []
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
