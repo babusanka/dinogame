774c568b20bc5bf6eeeffbb855e369ea4ef4d476
babusanka <saintsbabu@gmail.com>
2020-07-30 20:06:37 +0200

    added bullets :bullets are randomly shot out at "spawn" area :not following the player
    added WIP chatbox
    :just doesnt work lmao

--- app.js
+++ app.js
@@ -13,34 +13,135 @@ serv.listen(2000)
 console.log("Server Starded. (finally)")
 
 var SOCKET_LIST = {}
-var PLAYER_LIST = {}
 
-var Player = function(id){
+var Entity = function(){
     var self = {
         x:250,
         y:250,
-        id:id, 
-        number:"" + Math.floor(10 * Math.random()),
-        pressingRight:false,
-        pressingLeft:false,
-        pressingUp:false,
-        pressingDown:false,
-        maxSpd:20,
-
+        spdX:0,
+        spdY:0,
+        id:"",
     }
-    self.updatePostion = function(){
-        if(self.pressingRight)
-            self.x += self.maxSpd
-        if(self.pressingLeft)
-            self.x -= self.maxSpd
-        if(self.pressingUp)
-            self.y -= self.maxSpd
-        if(self.pressingDown)
-            self.y += self.maxSpd    
 
+self.update = function(){
+        self.updatePostion()
+    }
+
+    self.updatePostion = function(){ 
+        self.x += self.spdX
+        self.y += self.spdY
     }
     return self;
 }
+
+
+var Player = function(id){
+    var self = Entity()
+    self.id = id
+    self.number = "" + Math.floor(10 * Math.random()),
+    self.pressingRight = false
+    self.pressingLeft = false
+    self.pressingUp = false
+    self.pressingDown = false
+    self.maxSpd = 10
+
+    var super_update = self.update
+    self.update = function(){
+        self.updateSpd()
+        super_update()
+        
+    }
+
+
+    self.updateSpd = function(){
+        if(self.pressingRight)
+            self.spdX = self.maxSpd
+        else if(self.pressingLeft)
+            self.spdX = - self.maxSpd
+        else
+            self.spdX = 0
+
+        if(self.pressingDown)
+            self.spdY = self.maxSpd
+        else if(self.pressingUp)
+            self.spdY = - self.maxSpd
+        else
+            self.spdY = 0 
+    }
+    Player.list[id] = self 
+    return self;
+}
+Player.list = {}
+Player.onConnect = function(socket){
+    var player = Player(socket.id)
+    socket.on('keyPress',function(data){
+        console.log(data)
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state
+    })
+}
+Player.onDisconnect = function(socket){
+        delete Player.list[socket.id];
+}       
+Player.update = function(){
+    var pack = []
+    for(var i in Player.list){
+        var player = Player.list[i]
+        player.update()
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        })         
+    }
+    return pack;
+}
+
+
+var Bullet = function(angle){
+    var self = Entity()
+    self.id = Math.random()
+    self.spdX = Math.cos(angle/180*Math.PI) * 10
+    self.spdY = Math.sin(angle/180*Math.PI) * 10
+
+    self.timer = 0
+    self.toRemover = false
+    var super_update = self.update
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemover = true
+        super_update();
+
+    }
+    Bullet.list[self.id] = self
+    return self
+}
+Bullet.list = {}
+
+Bullet.update = function(){
+    if(Math.random() < 0.1){
+        Bullet(Math.random()*360)
+
+    }
+
+    var pack = []
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i]
+        bullet.update()
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        })         
+    }
+    return pack;
+}
+
 
 var io = require('socket.io')(serv,{})
 io.sockets.on('connection', function(socket){
@@ -50,43 +151,34 @@ io.sockets.on('connection', function(socket){
     socket.number = "" + Math.floor(10 * Math.random())
     SOCKET_LIST[socket.id] = socket
 
-    var player = Player(socket.id)
-    PLAYER_LIST[socket.id] = player
-
+    Player.onConnect(socket)
 
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id]
-        delete PLAYER_LIST[socket.id]
-        
+        Player.onDisconnect(socket)
+    })
+    socket.on('sendMsgToServer',function(data){
+        var playerName = ("" + socket.id).slice(2,7)
+        for(var i in SOCKET_LIST){
+            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data)
+
+        }
 
     })
 
-    socket.on('keyPress',function(data){
-        if(data.inputId === 'left')
-            player.pressingLeft = data.state
-        else if(data.inputId === 'right')
-            player.pressingRight = data.state
-        else if(data.inputId === 'up')
-            player.pressingDown = data.state
-        else if(data.inputId === 'down')
-            player.pressingUp = data.state
-    })
 })
 
 
+
 setInterval(function(){
-    var pack = []
-    for(var i in PLAYER_LIST){
-        var player = PLAYER_LIST[i]
-        player.updatePostion()
-        pack.push({
-            x:player.x,
-            y:player.y,
-            number:player.number
-        }) 
-            
+    var pack = {
+        player:Player.update(),
+        bullet:Bullet.update(),
 
     }
+
+        
+
 
     for(var i in SOCKET_LIST){
         var socket = SOCKET_LIST[i]
@@ -97,8 +189,6 @@ setInterval(function(){
 
 
 
-
-
-
     
 }, 1000/25)
+
