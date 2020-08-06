249c0e9531f9f558591bfd01a7eb516ed0d0b2a9
babusanka <saintsbabu@gmail.com>
2020-08-06 12:52:12 +0200

    lol

--- app.js
+++ app.js
@@ -204,70 +204,72 @@ var addUser = function(data, cb) {
 
 var io = require('socket.io')(serv, {})
 io.sockets.on('connection', function(socket) {
-            socket.id = Math.random()
-            socket.number = "" + Math.floor(10 * Math.random())
-            SOCKET_LIST[socket.id] = socket
+    socket.id = Math.random()
+    socket.number = "" + Math.floor(10 * Math.random())
+    SOCKET_LIST[socket.id] = socket
 
-            socket.on('signIn', function(data) {
-                isValidPassword(data, function(res) {
-                    if (res) {
-                        Player.onConnect(socket);
-                        socket.emit('signInResponse', { success: true });
-                    } else {
-                        socket.emit('signInResponse', { success: false });
-                    }
-
-                })
-            })
-
-            socket.on('signUp', function(data) {
-                    isUsernameTaken(data, function(res) {
-                            if (res) {
-                                socket.emit('signUpResponse', { success: false });
-                            } else {
-                                addUser(data, function() {
-                                    socket.emit('signUpResponse', { success: true });
-                                })
-
-                            })
-                    })
-
-
-                socket.on('disconnect', function() {
-                    delete SOCKET_LIST[socket.id]
-                    Player.onDisconnect(socket)
-                }) socket.on('sendMsgToServer', function(data) {
-                    var playerName = ("" + socket.id).slice(2, 7)
-                    for (var i in SOCKET_LIST) {
-                        SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data)
-
-                    }
-
-                })
-
-            })
-
-
-
-        setInterval(function() {
-            var pack = {
-                player: Player.update(),
-                bullet: Bullet.update(),
-
+    socket.on('signIn', function(data) {
+        isValidPassword(data, function(res) {
+            if (res) {
+                Player.onConnect(socket);
+                socket.emit('signInResponse', { success: true });
+            } else {
+                socket.emit('signInResponse', { success: false });
             }
 
+        })
+    })
 
-
-
-            for (var i in SOCKET_LIST) {
-                var socket = SOCKET_LIST[i]
-                socket.emit('newPositions', pack)
-
+    socket.on('signUp', function(data) {
+        isUsernameTaken(data, function(res) {
+            if (res) {
+                socket.emit('signUpResponse', { success: false });
+            } else {
+                addUser(data, function() {
+                    socket.emit('signUpResponse', { success: true });
+                })
             }
 
+        })
+    })
+
+
+    socket.on('disconnect', function() {
+        delete SOCKET_LIST[socket.id]
+        Player.onDisconnect(socket)
+    })
+    socket.on('sendMsgToServer', function(data) {
+        var playerName = ("" + socket.id).slice(2, 7)
+        for (var i in SOCKET_LIST) {
+            SOCKET_LIST[i].emit('addToChat', playerName + ': ' + data)
+
+        }
+
+    })
+
+})
+
+
+
+setInterval(function() {
+    var pack = {
+        player: Player.update(),
+        bullet: Bullet.update(),
+
+    }
+
+
+
+
+    for (var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i]
+        socket.emit('newPositions', pack)
+
+    }
 
 
 
 
 
-        }, 1000 / 25)
+
+}, 1000 / 25)
